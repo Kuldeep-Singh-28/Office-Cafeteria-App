26c7cd8bf82bbb4f50e68a320ae3680dd8c6029b
DEEPIKA-SUK <sukhijadeepika2001@gmail.com>
2021-01-15 22:52:06 +0530

    Update contact page

--- app/http/controllers/homecontroller.js
+++ app/http/controllers/homecontroller.js
@@ -1,5 +1,14 @@
 const Menu = require("../../models/menu");
-
+// Deepika------start----
+const nodemailer = require("nodemailer");
+const transporter = nodemailer.createTransport({
+  service: "gmail",
+  auth: {
+    user: "dummy mail",
+    pass: "",
+  },
+});
+// Deepika------end------
 const homeController = () => {
   // factory functions
   return {
@@ -11,6 +20,26 @@ const homeController = () => {
       const foods = await Menu.find();
       return res.render("menu", { foods: foods });
     },
+    // Deepika -----start-----
+    contact_us: async (req, res) => {
+      const { name, email, message } = req.body;
+      var mailOptions = {
+        from: "alphacafetria@gmail.com",
+        to: email,
+        subject: "Thanks for contacting us!",
+        html: `<p>Hello ${name}</p><p>Thanks for contacting us! This auto-reply is just to let you know that we have received your email and will get back to you with a (human) response as soon as possible.</p>
+        <p>Cheers!</p>`,
+      };
+      transporter.sendMail(mailOptions, function (error, info) {
+        if (error) {
+          console.log(error);
+        } else {
+          console.log("Email sent: " + info.response);
+        }
+      });
+      return res.redirect("/");
+    },
+    // Deepika ------end------
   };
 };
 
